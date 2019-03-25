fcb0775691ed52a13f1c6048892bd1c675ad8229
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-03-25 15:33:33 -0400

    API key set within .gitignore

--- webplayer-lite/src/App.js
+++ webplayer-lite/src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 class App extends Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      artist: '',
+      tracks: '',
+      current: {}
+    }
+  }
+
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  fetchData(){
+    const KEY = process.env.REACT_APP_SCLOUD_API_KEY
+    fetch(`http://api.soundcloud.com/tracks?client_id=${KEY}&q`)
+    .then(response => response.json())
+    .then(parsedJSON => console.log(parsedJSON.results))
+    .catch(error => console.log('parsing failed'))
+  }
+
+
   render() {
+
     return (
       <div className="App">
         <h1>SoundCloud WebPlayer - App.js</h1>        
