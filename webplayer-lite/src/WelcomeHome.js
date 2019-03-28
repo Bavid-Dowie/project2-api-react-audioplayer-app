b973a5d549dc5faf1ab492384d7f8c796e5ef5d1
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-03-28 18:53:59 -0400

    Git issues resolved

--- webplayer-lite/src/WelcomeHome.js
+++ webplayer-lite/src/WelcomeHome.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
-=======
 import Main from './Main'
->>>>>>> 16d03589ebf8bbfd2bb1d7237004995faac4b2ba
 import './WelcomeHome.css'
 
 export default class WelcomeHome extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-=======
         <Main />
->>>>>>> 16d03589ebf8bbfd2bb1d7237004995faac4b2ba
         You landed on the landing page!
       </div>
     )
